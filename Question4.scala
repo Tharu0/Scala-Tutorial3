0a4e58cf73c5eca3b9a2fc79ba54b81c4dcaf436
Tharu0 <87253039+Tharu0@users.noreply.github.com>
2022-07-18 17:07:03 +0530

    Update Question4.scala

--- Question4.scala
+++ Question4.scala
@@ -2,7 +2,8 @@ object Q4 extends App{
 	def total(a:Int):Double=a*24.95
 	def discount(b:Double):Double=b*0.4 
 	def shippingCost(a:Int):Double=a match{
-               case x if x>50 => 3*a+(a-50)*.75
+            case x if x>=50 => 3*50+(a-50)*.75
+            case x if x<50 => 3*a
         }
 	var amount=total(60)
 	printf("%.3f",amount-discount(amount)+shippingCost(60))
